d4c289b2096e47575e41bd61e3432a768895c425
Zain Ahmad <ZainAhmad@gmail.com>
2025-10-17 21:07:37 +0500

    Dev2 modified login route differently

--- housepk_app.py
+++ housepk_app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Welcome to HousePK Flask App - Main Branch"
+@app.route('/login')
+def login():
+    return "Login Page - by Dev2 (different implementation)"
 
 if __name__ == '__main__':
     app.run(debug=True)
