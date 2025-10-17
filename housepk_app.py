bdb0d8a40a9b49fa33e30f36be9c77ccf55c58dc
Zain Ahmad <ZainAhmad@gmail.com>
2025-10-17 21:18:51 +0500

    Added /dashboard route after merge

--- housepk_app.py
+++ housepk_app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/login')
-def login():
-    return "Merged Login Page - combined Dev1 & Dev2"
+@app.route('/dashboard')
+def dashboard():
+    return "Dashboard Page - added after merge"
 
 
 if __name__ == '__main__':
